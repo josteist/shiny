d79729d35fae219b41ecb1d73f4932bbda9e0e87
Grøneng <GryMarysol.Groneng@fhi.no>
2020-09-17 15:26:24 +0200

    Text changes

--- ext/beta_kht/news_faq.R
+++ ext/beta_kht/news_faq.R
@@ -17,6 +17,13 @@ news_faq_ui <- function(id, config) {
           "Øverst på denne nettsiden er det tre faner; 'Nytt fra FHI', 'Covid-19' og 'NorSySS'.",
           "Om du trykker på 'Covid-19' vil du komme til siden om Covid-19 (den tidligere startsiden på dette nettstedet)", br(), br(),
 
+
+          strong("Hvordan kan jeg få en liste over totalt antall covid-19 tilfeller i et geografisk område?"), br(),
+          "Vi jobber med å lage en kolonne med totalt antall i Tabell 1.", br(),
+           "Som en midlertidig løsning kan du laste ned tabellen for figur 1,", br(),
+          " og summere alle i MSIS kolonnen i excelarket.", br(),br(),
+
+
           strong("Hvor ofte oppdateres dataene i nettsiden?"), br(),
           "NorSySS dataene blir oppdatert på nettsiden ca kl 13 hver dag.", br(),
           "MSIS, MSIS laboratoriedataene og NoPaR dataene blir oppdatert",
--- ext/beta_kht/news_news.R
+++ ext/beta_kht/news_news.R
@@ -11,16 +11,29 @@ news_news_ui <- function(id, config) {
       column(
         width=10, align="left",
         p(
-          strong("02.09.2020 Opptak av webinar"), br(), br(),
+
+          strong("09.09.2020 Opptak av webinar om influensavaksinasjon"), br(),
+          "Onsdag 9. september ble det avholdt et webinar om influensavaksinasjon.", br(),
+
+          a(href="https://www.fhi.no/meldinger/opptak-fra-webinaret-om-influensavaksinasjon/", "Trykk HER for å se opptak av webinaret"),br(), br(),
+
+
+          strong("02.09.2020 Opptak av webinar om covid-19 og smitteverntiltak for kommuneleger"), br(),
           "Folkehelseinstituttet arrangerte torsdag 27. august kl. 10.00 - 11.30",
-          " et webinar med spørretime om covid-19 og smitteverntiltak for kommuneleger.",br(), br(),
+          " et webinar med spørretime om covid-19 og smitteverntiltak for kommuneleger.",br(),
 
           " Dette var det andre seminaret i rekken av webinar med spørretime for kommuneleger.",
           " Seminaret startet med noen korte presentasjoner om håndtering av ulike typer covid-19-utbrudd,",
-          " før fagfolk ved FHI svarte på spørsmål.", br(), br(),
+          " før fagfolk ved FHI svarte på spørsmål.", br(),
 
           a(href="https://vimeo.com/453407494/bc1e531271", "Trykk HER for å se opptak av webinaret"),br(), br(),
 
+
+          strong("Covid-19 ukerapport fra FHI"), br(),
+          a(href="https://www.fhi.no/publ/2020/koronavirus-ukerapporter/", "Trykk HER for å se covid-19 ukerapportene"),br(), br(),
+
+          strong("Covid-19 matematisk modellering rapport fra FHI"), br(),
+          a(href="https://www.fhi.no/sv/smittsomme-sykdommer/corona/koronavirus-modellering/", "Trykk HER for å se covid-19 modelleringsrapportene"),br(), br(),
 
           strong(""), br(), br()
 
